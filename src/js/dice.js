079fe57106acf1efaee9821c21085ec1e2420505
Sakumii <sakumimi10@homtail.fr>
2022-11-03 14:43:35 +0100

    player function start coding

--- src/js/dice.js
+++ src/js/dice.js
@@ -1,9 +1,12 @@
-
 //Dice function//
 
 let images = ["images/dice-01.svg", "images/dice-02.svg", "images/dice-03.svg", "images/dice-04.svg", "images/dice-05.svg", "images/dice-06.svg"];
 
 let dice = document.querySelectorAll("img");
+
+function _(e){
+    document.getElementById(e);
+}
 
 function roll(){
     dice.forEach(function(die){

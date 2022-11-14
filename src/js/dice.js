b05ca8880d4482015f25eba3ec2637970a8d0210
Sakumi <sakumimi10@hotmail.fr>
2022-11-14 13:01:43 +0100

    add img and js

---
+++ src/js/dice.js
@@ -0,0 +1,27 @@
+//Dice function//
+
+let images = ["images/dice-01.svg", "images/dice-02.svg", "images/dice-03.svg", "images/dice-04.svg", "images/dice-05.svg", "images/dice-06.svg"];
+
+let dice = document.querySelectorAll("img");
+
+function _(e){
+    document.getElementById(e);
+}
+
+function roll(){
+    dice.forEach(function(die){
+        die.classList.add("shake");
+    });
+    setTimeout(function() {
+        dice.forEach(function(die){
+            die.classList.remove("shake");
+        });
+        let dieOneValue = Math.floor(Math.random()
+        * (7 - 1) + 1);
+        console.log(dieOneValue);
+        document.querySelector("#die-1").setAttribute("src", images[dieOneValue - 1]);
+        document.querySelector("#round_p1").innerHTML = (dieOneValue);
+    },
+    2000
+    );
+}
---
+++ src/js/new_game.js
@@ -0,0 +1,7 @@
+//New game function//
+const newGame = _('new_game')
+
+function _(e){
+    document.getElementById(e);
+}
+

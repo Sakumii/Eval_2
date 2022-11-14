458f713678381f6e1575cf5aa15e41d870f53497
Sakumi <sakumimi10@hotmail.fr>
2022-11-14 16:41:20 +0100

    2 nd try

--- src/js/new_game.js
+++
@@ -1,7 +0,0 @@
-//New game function//
-const newGame = _('new_game')
-
-function _(e){
-    document.getElementById(e);
-}
-
--- src/js/players.js
+++
@@ -1,7 +0,0 @@
-const playerOne = _('player_1');
-const playerTwo = _('player_2');
-
-function _(e){
-    document.getElementById(e);
-}
-
